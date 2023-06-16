4472513d5b58ff9e9cce10c436fb5df1bdab955c
Pedro dos Anjos <pedrodeanjos@gmail.com>
2023-06-16 16:12:29 -0300

    flash messages

--- Projeto Toughts/routes/authRoutes.js
+++ Projeto Toughts/routes/authRoutes.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const router= express.Router()
 const AuthController = require('../controllers/AuthController')
-// controller 
+
 
 router.get('/login', AuthController.login)
 router.get('/register', AuthController.register)
+router.post('/register', AuthController.registerPost)
+
 
 
 module.exports = router
