bff503a5a140edcdd1b0244057e1571e89c51f92
Pedro dos Anjos <pedrodeanjos@gmail.com>
2023-06-20 00:28:16 -0300

    resgatando pensamentos para dashboard

--- Projeto Toughts/controllers/ToughtController.js
+++ Projeto Toughts/controllers/ToughtController.js
@@ -7,7 +7,28 @@ module.exports = class ThoughtsController {
     }
 
     static async dashboard(req, res) {
-        res.render('toughts/dashboard')
+        const userId = req.session.userid
+
+        const user = await User.findOne({
+            where: {
+                id: userId,
+            },
+            include: Tought,
+            plain: true,
+        })
+
+
+        //check if user exist 
+        if (!user) {
+            res.redirect('/login')
+        }
+
+        const toughts = user.Toughts.map((result) => result.dataValues)
+
+        console.log(user.Toughts)
+
+
+        res.render('toughts/dashboard', {toughts})
     }
 
     static createTought (req, res) {
