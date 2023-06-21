30952f5b13a57eed2fb9ffa3af048c30f9602895
Pedro dos Anjos <pedrodeanjos@gmail.com>
2023-06-21 00:06:46 -0300

    remove thought

--- Projeto Toughts/controllers/ToughtController.js
+++ Projeto Toughts/controllers/ToughtController.js
@@ -57,4 +57,25 @@ module.exports = class ThoughtsController {
             console.log('Aconteceu um erro' + error)
         }
     }
+
+    static async removeTought(req, res){
+
+        const id = req.body.id
+        const UserId = req.session.userid
+
+    try {
+        
+        await Tought.destroy({where: {id: id, UserId:UserId}})
+
+
+        req.flash('message', 'Pensamento removido com sucesso!')
+
+        req.session.save(()=>{
+            res.redirect('/toughts/dashboard')
+        })
+
+    } catch (error) {
+        console.log('Aconteceu um erro: ' + error)
+    }
+    }
 }
--- Projeto Toughts/routes/toughtsRoutes.js
+++ Projeto Toughts/routes/toughtsRoutes.js
@@ -8,6 +8,7 @@ const checkAuth = require('../helpers/auth').checkAuth
 router.get('/add',checkAuth, ThoughtController.createTought)
 router.post('/add',checkAuth, ThoughtController.createToughtSave)
 router.get('/dashboard',checkAuth, ThoughtController.dashboard)
+router.post('/remove', checkAuth, ThoughtController.removeTought) 
 router.get('/', ThoughtController.showThoughts)
 
 module.exports = router
